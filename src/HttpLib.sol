21497fd56de15648f2fed1be45c4be96ac0f3074
steven <steven.nevins.js@gmail.com>
2024-10-10 12:44:31 -0400

    first commit

---
+++ src/HttpLib.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {stdJson} from "forge-std/StdJson.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+library HttpLib {
+    using stdJson for string;
+
+    Vm constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+    function get(string memory url) internal returns (string memory) {
+        string[] memory inputs = new string[](3);
+        inputs[0] = "curl";
+        inputs[1] = "-s"; // Silent mode
+        inputs[2] = url;
+
+        bytes memory result = vm.ffi(inputs);
+
+        return string(result);
+    }
+
+    function post(string memory url, string memory data) internal returns (string memory) {
+        string[] memory inputs = new string[](7);
+        inputs[0] = "curl";
+        inputs[1] = "-s";
+        inputs[2] = "-X";
+        inputs[3] = "POST";
+        inputs[4] = "-d";
+        inputs[5] = data;
+        inputs[6] = url;
+
+        bytes memory result = vm.ffi(inputs);
+
+        return string(result);
+    }
+
+    function getWithHeaders(string memory url, string[] memory headers) internal returns (string memory) {
+        uint256 headerCount = headers.length;
+        string[] memory inputs = new string[](3 + headerCount * 2);
+        inputs[0] = "curl";
+        inputs[1] = "-s";
+
+        uint256 index = 2;
+
+        for (uint256 i = 0; i < headerCount; i++) {
+            inputs[index] = "-H";
+            index += 1;
+            inputs[index] = headers[i];
+            index += 1;
+        }
+
+        inputs[index] = url;
+
+        bytes memory result = vm.ffi(inputs);
+
+        return string(result);
+    }
+
+    function postWithHeaders(
+        string memory url,
+        string memory data,
+        string[] memory headers
+    ) internal returns (string memory) {
+        uint256 headerCount = headers.length;
+        string[] memory inputs = new string[](7 + headerCount * 2);
+        inputs[0] = "curl";
+        inputs[1] = "-s";
+        inputs[2] = "-X";
+        inputs[3] = "POST";
+
+        uint256 index = 4;
+
+        for (uint256 i = 0; i < headerCount; i++) {
+            inputs[index] = "-H";
+            index += 1;
+            inputs[index] = headers[i];
+            index += 1;
+        }
+
+        inputs[index] = "-d";
+        inputs[index + 1] = data;
+        inputs[index + 2] = url;
+
+        bytes memory result = vm.ffi(inputs);
+
+        return string(result);
+    }
+}
---
+++ test/HttpLib.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {HttpLib} from "../src/HttpLib.sol";
+
+contract HttpLibTest is Test {
+    using stdJson for string;
+
+    function testGetRequest() public {
+        string memory response = HttpLib.get("https://example-data.draftbit.com/products?_limit=1");
+        emit log_string(response);
+    }
+
+    function testPostRequest() public {
+        string memory data = '{"name":"New Product","description":"A test product"}';
+        string memory response = HttpLib.post("https://example-data.draftbit.com/products", data);
+        emit log_string(response);
+    }
+
+    function testGetRequestWithHeaders() public {
+        string[] memory headers = new string[](1);
+        headers[0] = "Accept: application/json";
+
+        string memory response = HttpLib.getWithHeaders("https://example-data.draftbit.com/products?_limit=1", headers);
+        emit log_string(response);
+    }
+
+    function testPostRequestWithHeaders() public {
+        string memory data = '{"name":"New Product","description":"A test product"}';
+
+        string[] memory headers = new string[](1);
+        headers[0] = "Content-Type: application/json";
+
+        string memory response = HttpLib.postWithHeaders("https://example-data.draftbit.com/products", data, headers);
+        emit log_string(response);
+    }
+
+    function testParseJsonResponse() public {
+        string memory response = HttpLib.get("https://example-data.draftbit.com/products?_limit=1");
+        string memory productName = response.readString("$[0].name");
+        emit log_string(productName);
+    }
+}
